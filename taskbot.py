9acf085334505189c40258715f00e43aa41db43a
Stephen Dop <dopstephen@gmail.com>
2015-09-20 22:41:38 +0100

    Cleanup

--- taskbot.py
+++ taskbot.py
@@ -2,11 +2,23 @@
 # -*- coding:  utf-8 -*-
 
 import logging
+import datetime
 from Bot.TheBot import Bot
 from Bot.TasksManager import TaskManager
 from Tasks.r_rsisubscribers.SubscriberTasks import CheckSubscriberMessagesTask, AuthenticateSubscribersTask
 from Bot.Exceptions import MessageNotFoundException
-error_count = 0
+
+""" init log """
+LOGFORMAT = '%(asctime)-15s %(message)s \n\n'
+today = datetime.date.today()
+logfile = "Logs/" + today.strftime('%d-%b-%Y') + ".log"
+logging.basicConfig(
+    filename=logfile,
+    filemode="w",
+    level=logging.WARN,
+    format=LOGFORMAT
+)
+logging.captureWarnings(True)
 
 try:
     tasks = [CheckSubscriberMessagesTask, AuthenticateSubscribersTask]
@@ -22,5 +34,4 @@ except MessageNotFoundException as message_not_found:
 except Exception as error:
     # Todo on a fatal error send message to bot owner and dev
     logging.exception(error)
-    error_count += 1
     raise
