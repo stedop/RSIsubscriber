9f36c27e01fe54ef24a1ffcd53ecb97bf65a17c0
David Tran <david.tran@trandav.com>
2015-12-01 23:38:22 -0500

    Forgot to add the actual bot script

---
+++ flairbot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:  utf-8 -*-
+
+import logging
+import datetime
+from Bot.TheBot import BNBot
+from Bot.TasksManager import TaskManager
+from Tasks.r_starcitizen_trades.PrivateMessageTask import ConfirmCitizenTask
+import os
+
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+""" init log """
+today = datetime.date.today()
+logfile = BASEDIR + "/Logs/flairbot-" + today.strftime('%d-%b-%Y') + ".log"
+
+LOGFORMAT = '\n%(asctime)s - %(name)s - %(levelname)s - %(message)s\n'
+DATEFORMAT = '%Y-%m-%d %H:%M:%S'
+logging.basicConfig(
+    filename=logfile,
+    filemode="a",
+    level=logging.WARN,
+    format=LOGFORMAT,
+    datefmt=DATEFORMAT
+)
+logging.captureWarnings(True)
+bot_logger = logging.getLogger('Flairbot')
+
+""" Main """
+try:
+    tasks = [ConfirmCitizenTask]
+    myBot = BNBot(BASEDIR, 'flairbot_config.ini', bot_logger)
+    task_manager = TaskManager(tasks, myBot)
+    task_manager.run()
+
+except Exception as error:
+    # Todo on a fatal error send message to bot owner and dev
+    bot_logger.exception(error)
+    pass
